fd042f577c3d0593c878d7095757a67f19ea4b92
Joseph <ruizjoseph2002@gmail.com>
2023-04-03 12:01:21 -0500

    agregue mis archivos del portfolio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Header toggle
+
+let MenuBtn = document.getElementById("MenuBtn")
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-activate');
+    this.classList.toggle('fa-xmark')
+})
+
+
+//Typing Effect
+
+let typed =  new Typed('auto-input',{
+    strings: ['Front-END Developer junior!', 'Developer junior',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
